0af2f16b8ebb965aa94bef3f2a65dd1ad3588c42
ASW <ASW@192.168.0.14>
2018-12-05 10:31:48 +0100

    The linec chart is included but usign local db connection and not webservice.

--- src/guimoniasthma/MoniAstmaController.java
+++ src/guimoniasthma/MoniAstmaController.java
@@ -6,11 +6,22 @@
 package guimoniasthma;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
@@ -39,6 +50,21 @@ public class MoniAstmaController implements Initializable {
     private TableView<Peakflow> tableView; 
     
     
+    //Line chart
+    
+    
+     @FXML
+    final CategoryAxis xAxisPF = new CategoryAxis();
+    
+    @FXML
+    final NumberAxis yAxisPF = new NumberAxis();
+    
+    @FXML 
+    LineChart<String, Number> chartPFdata; 
+    
+    
+    //Methods
+    
     @FXML
    private void handleSearchAction(){
        WebTarget clientTarget;
@@ -66,6 +92,39 @@ public class MoniAstmaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         handleSearchAction();
+        
+//        WebTarget clientTarget;
+//        
+//        Client client = ClientBuilder.newClient();
+//        
+//        clientTarget = client.target("http://localhost:8080/ServerSideMoniAsthma/webresources/peakflow");
+//        
+
+        
+        
+             Connection con1;
+        try{
+            con1 = DriverManager.getConnection("jdbc:derby://localhost:1527/fifi", "fifi", "fifi");
+            Statement stmt = con1.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM FIFI.PEAKFLOW");
+            //FETCH FIRST 3 ROWS ONLY");
+            
+            LineChart.Series<String,Number> series10 = new LineChart.Series<String,Number>();
+            while(rs.next())
+            {
+                
+                series10.getData().add(new XYChart.Data<String,Number>(rs.getDate(3).toString(),rs.getInt(2)));
+                
+            } 
+
+             chartPFdata.getData().add(series10);
+          
+        }
+        catch (SQLException ex){
+            Logger.getLogger(GUIMoniAsthma.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        
+        
     }    
     
 }
